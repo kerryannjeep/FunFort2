1cada742338e0763ae708240f04a2b219bdba49c
DJ <umdjm@hotmail.com>
2013-11-17 15:48:17 -0500

    Fix for Message Model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   attr_accessible :friendship_id, :data, :parent_approval, :friend_parent_approval, :expiry_date, :child_id, :friend_id
   belongs_to :friendship
   belongs_to :child
-  belongs_to :friend
+  belongs_to :friend, :class_name => "Child"
 
   after_initialize do
     self.parent_approval = false unless self.parent_approval
