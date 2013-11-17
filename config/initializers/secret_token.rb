7c0b35d008140efcfc8d315f8f9f2c7859916b55
kerryannjeep <kschaffer@marketingassociates.com>
2013-11-16 20:28:06 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FunFort::Application.config.secret_key_base = 'ce20b4162697f6300fa8042381043f158178f6b7243dab6f5ff126b2d3e3f236dbcdbd0d761e1cfd17cda51507daf242076c59dcb82722044fb12fae8c597617'
