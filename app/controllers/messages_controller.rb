5b408aa2afbee0dcff7612fd631b9d57c17e8420
DJ <umdjm@hotmail.com>
2013-11-17 15:59:43 -0500

    fix url

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_filter :authorize
 
   # GET /messages
   # GET /messages.json
