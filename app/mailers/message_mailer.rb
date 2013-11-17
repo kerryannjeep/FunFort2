2e45bbe7962a7e1ebfa4e6d10a369ee2c0cf997d
DJ <umdjm@hotmail.com>
2013-11-17 15:42:35 -0500

    Fix for mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,8 +3,6 @@ class MessageMailer < ActionMailer::Base
 
   def approval_email(message)
     @message = message
-    @approve_url  = root_url + "/approvals/#{@message.id}/confirm"
-    @reject_url = root_url + "/approvals/#{@message.id}/reject"
     mail(to: @message.child.parent.email, subject: "#{@message.child.first_name} sent a message, Please review")
   end
 end
